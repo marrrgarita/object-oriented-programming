b1928a697b8c596987977284b6c4c035933de474
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-29 17:23:36 -0400

    fix error - require the class file, not source the class file

--- cats-function.rb
+++ cats-function.rb
@@ -1,11 +1,12 @@
-source './cats-meow.rb'
+require './cats-meow.rb'
 
 whiskers = Cat.new("Whiskers", "Meow Mix", 21)
 mittens = Cat.new("Mittens", "Kitty Chow", 12)
 
-whiskers.eats_at
-mittens.eats_at
+puts whiskers.eats_at
+puts mittens.eats_at
 
-
+puts whiskers.meow
+puts mittens.meow
 
 puts "finished!"
