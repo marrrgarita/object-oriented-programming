edc8359e72553c218627b14e55508bb1a9a2c220
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-30 08:10:06 -0400

    deliver method to calculate earnings based on papers delivered

--- paperboy.rb
+++ paperboy.rb
@@ -9,4 +9,24 @@ class Paperboy
     @quota = 0
   end
 
-  
+  def quota
+    @quota = (@experience/2) + 50
+    "#{@name}, you must deliver #{@quota} papers today"
+  end
+
+  def deliver(house_start, house_end)
+    papers = house_end - house_start
+
+    if papers < @quota
+      @earnings += (0.25 * papers) - 2
+      @experience += papers
+
+    else
+      @earnings += (0.25 * @quota) + ((papers - @quota) * 0.5)
+      @experience += papers
+    end
+
+    return @earnings
+  end
+
+end
